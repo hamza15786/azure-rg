27caebd4cf1825aaf39eece7e82986b61b2b7714
hamza15786 <43768836+hamza15786@users.noreply.github.com>
2021-07-08 07:42:04 -0500

    Add files via upload

---
+++ example.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+
+backend "remote" {
+     organization = "example-org-4f064f"
+     workspaces {
+                 name = "Example-Workspace"
+
+}
+}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
